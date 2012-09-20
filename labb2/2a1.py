aaf336992620dc803e88e5b3a1bf78262b633b17
Anton <bantonlindgren@gmail.com>
2012-09-20 20:40:58 +0200

    deleted :)

--- labb2/2a1.py
+++
@@ -1,8 +0,0 @@
-def asterix():
-    """Prints a textbox with a nice asterix frame"""
-    print("*" * (len(text) + 4))
-    print("* " + text + " *")
-    print("*" * (len(text) + 4))
-
-text = input("Mata in en text: ")	#prompts the user to input something
-asterix()				#Calls the asterix function
